917f8d74c6b704b4301dec3458d73e0654c857b6
Amanda Lima <unexamanda@gmail.com>
2026-02-16 20:29:04 -0300

    Uptade database
    I created the database and created the first two tables.

--- databasesql/bookdatabase.sql
+++ databasesql/bookdatabase.sql
@@ -1,1 +1,17 @@
+create database book_odyssey
 
+create table books (
+id_books int not null, 
+name_books varchar(100) not null,
+name_authors varchar(80) not null,
+constraint pk_id_books primary key (id_books)
+);
+
+
+create table publishers(
+id_publishers int not null,
+name_publishers varchar(100) not null,
+name_writers varchar (80) not null,
+number_works int not null,
+constraint pk_id_publishers primary key (id_publishers)
+);
