985cb4a0c3de47c66621d50a0a3d54737b7a358f
Faby_mfsm <martinsfabiana536@gmail.com>
2026-02-17 18:31:53 -0300

    adding the book_condition table

--- databasesql/bookdatabase.sql
+++ databasesql/bookdatabase.sql
@@ -1,4 +1,4 @@
-create database book_odyssey
+create database book_odyssey;
 
 create table books (
 id_books int not null, 
@@ -18,8 +18,16 @@ constraint pk_id_publishers primary key (id_publishers)
 
 
 create table categories (
-id_category int primary key auto_increment,
+id_category int not null auto_increment,
 gender varchar(100) not null,
-language varchar(50) not null
+language varchar(50) not null,
+constraint pk_id_category primary key (id_category)
 );
 
+
+create table book_condition (
+id_condition int not null auto_increment,
+description varchar(50) not null,
+conservation decimal(5,2) not null,
+constraint pk_id_condition primary key (id_condition)
+);
