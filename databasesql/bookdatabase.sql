19464fd0638a8454f2cfe19e604792f482188b6d
Lismara Santos <llis003014@gmail.com>
2026-02-21 19:36:48 -0300

    Update bookdatabase.sql
    add livraria.supplier_shipping , .supplier , .admin_users

--- databasesql/bookdatabase.sql
+++ databasesql/bookdatabase.sql
@@ -81,3 +81,32 @@ redemption_points int not null,
 constraint pk_id_supplier_coupon primary key (id_supplier)
 );
 
+create table livraria.supplier_shipping (
+	id_supplier_shipping serial4 NOT NULL,
+	tracking_code varchar(50) NOT NULL,
+	shipping_date date NOT NULL,
+	shipping_value numeric(10, 2) NOT NULL,
+	CONSTRAINT supplier_shipping_pkey PRIMARY KEY (id_supplier_shipping),
+	CONSTRAINT supplier_shipping_tracking_code_key UNIQUE (tracking_code)
+);
+
+create table  livraria.supplier (
+	id_supplier serial4 NOT NULL,
+	name_supplier varchar(100) NOT NULL,
+	cpf bpchar(11) NOT NULL,
+	email varchar(100) NOT NULL,
+	phone varchar(15) NOT NULL,
+	address varchar(200) NOT NULL,
+	CONSTRAINT supplier_cpf_key UNIQUE (cpf),
+	CONSTRAINT supplier_pkey PRIMARY KEY (id_supplier)
+);
+
+create table  livraria.admin_users (
+	id_admin serial4 NOT NULL,
+	name_admin varchar(100) NOT NULL,
+	job_title varchar(50) NOT NULL,
+	email varchar(100) NOT NULL,
+	phone varchar(15) NOT NULL,
+	CONSTRAINT admin_users_email_key UNIQUE (email),
+	CONSTRAINT admin_users_pkey PRIMARY KEY (id_admin)
+);
