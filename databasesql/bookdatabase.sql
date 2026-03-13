ebd826e2019a5048f308dc4ed54c838ef4ebb4ca
Amanda Lima <unexamanda@gmail.com>
2026-03-12 21:06:54 -0300

    Refactoring
    Standardizing names, languages, and data types.

--- databasesql/bookdatabase.sql
+++ databasesql/bookdatabase.sql
@@ -1,46 +1,46 @@
 create database book_odyssey;
 
 create table books (
-	id_books int not null, 
+	id int not null, 
 	name_books varchar(100) not null,
 	name_authors varchar(80) not null,
-	constraint pk_id_books primary key (id_books)
+	constraint pk_id_book primary key (id)
 );
 
 
-create table publishers(
-	id_publishers int not null,
+create table publisher(
+	id int not null,
 	name_publishers varchar(100) not null,
 	name_writers varchar (80) not null,
 	number_works int not null,
-	constraint pk_id_publishers primary key (id_publishers)
+	constraint pk_id_publisher primary key (id)
 );
 
 
-create table categories (
-	id_category int not null,
+create table categorie (
+	id int not null,
 	gender varchar(100) not null,
 	language varchar(50) not null,
-	constraint pk_id_category primary key (id_category)
+	constraint pk_id_category primary key (id)
 );
 
 
 create table book_condition (
-	id_condition int not null,
+	id int not null,
 	condition_description varchar(50) not null,
 	price_depreciation decimal(5,2) not null,
-	constraint pk_id_condition primary key (id_condition)
+	constraint pk_id_condition primary key (id)
 );
 
 
-create table customers (
-    id_customers int not null,
+create table customer (
+    id int not null,
     name_customers varchar(100) not null, 
     cpf char(11) not null,
     Email varchar(100) not null,
     telefone varchar(15) not null,
     endereco varchar(200) not null,
-    constraint pk_id_customers primary key (id_customers)
+    constraint pk_id_customer primary key (id)
 );
 
 
@@ -74,31 +74,31 @@ create table customer_shipping (
 );
 
 
-create table supplier_coupons (
-	id_supplier int not null,
+create table supplier_coupon (
+	id int not null,
 	donated_books_quantity int not null,
 	redemption_points int not null,
-	constraint pk_id_supplier_coupon primary key (id_supplier)
+	constraint pk_id_supplier_coupon primary key (id)
 );
 
 create table livraria_supplier_shipping (
-	id_supplier_shipping serial4 not null,
+	id int not null,
 	tracking_code varchar(50) not null,
 	shipping_date date not null,
 	shipping_value numeric(10, 2) NOT NULL,
 	constraint supplier_shipping_tracking_code_key UNIQUE (tracking_code),
-	constraint supplier_shipping_pkey primary key (id_supplier_shipping)
+	constraint supplier_shipping_pkey primary key (id)
 );
 
-create table  livraria_supplier (
-	id_supplier serial not null,
+create table supplier (
+	id int not null,
 	name_supplier varchar(100) not null,
 	cpf bpchar(11) not null,
 	email varchar(100) not null,
 	phone varchar(15) not null,
 	address varchar(200) not null,
 	constraint supplier_cpf_key UNIQUE (cpf),
-	constraint supplier_pkey primary key (id_supplier)
+	constraint supplier_pkey primary key (id)
 );
 
 create table  livraria_admin_users (
